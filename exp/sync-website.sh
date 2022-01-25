c5c8aa97bba0ed5f6d3d6567cb9815e8abef7d62
nils-holmberg <nils.holmberg@outlook.com>
2022-01-25 17:02:52 +0100

    strategic communication with r

--- exp/sync-website.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-rsync -e "ssh -i ~/doc/env/ssh/id_rsa" -av --progress --stats --delete --force ~/dev/html/ceba-vis sol-nhl@sgsdm.isk.lu.se:/tmp/
-ssh -i ~/doc/env/ssh/id_rsa -t sol-nhl@sgsdm.isk.lu.se "sudo bash /tmp/ceba-vis/src/remote-commands.sh"
-
